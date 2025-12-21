464b6d46caa21f8358d607486da048e60fd68661
George Grainger <ggrainger34@gmail.com>
2025-12-21 14:29:16 +0000

    Solved 704
    Implement binary search algorithm to find target index.

---
+++ 704 - Binary Search.py
@@ -0,0 +1,31 @@
+"""
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+"""
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        l, r = 0, n - 1
+        while l <= r:
+            m = (l + r) // 2
+            print(l,m,r)
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            elif nums[m] > target:
+                r = m - 1
+        return -1
